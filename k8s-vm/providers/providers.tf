e22aac97e02a1283326a3d3fc175ac02366259a2
wonjin <126428788+smilewonjin@users.noreply.github.com>
2023-08-23 22:57:10 +0900

    Update providers.tf

--- k8s-vm/providers/providers.tf
+++ k8s-vm/providers/providers.tf
@@ -1,22 +1,20 @@
 terraform {
-  required_version = ">=0.12"
-
+  required_version = ">=1.0"
   required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "~>3.0"
     }
     random = {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
-    tls = {
-      source = "hashicorp/tls"
-      version = "~>4.0"
-    }
   }
 }
-
 provider "azurerm" {
   features {}
 }
